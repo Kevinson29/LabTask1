c1fddde3080932e7659b11d8f86f64454378ac95
Kevinson <kevinsonkruz@gmail.com>
2023-09-18 08:58:40 +0800

    first commit

---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        Me.Hide()
+        AboutBox1.Show()
+    End Sub
+End Class
